a2cd3c90469f67ec6af598dfcc686d57f41bbb71
Francys999 <francis11444@gmail.com>
2024-11-14 15:43:46 -0500

    Agregando el CRUD Crear nuevo producto y sus correspendientes alertas

--- app/Livewire/Admin/Products/ProductCreate.php
+++ app/Livewire/Admin/Products/ProductCreate.php
@@ -3,13 +3,20 @@
 namespace App\Livewire\Admin\Products;
 
 use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Symfony\Component\CssSelector\Node\FunctionNode;
 
 class ProductCreate extends Component
 {
+    use WithFileUploads;
 
     public $categories;
-    
+    public $category_id = "";
+
+    public $image;
+
     public $product = [
         "sku" => "",
         "name" => "",
@@ -22,6 +29,49 @@ class ProductCreate extends Component
     public function mount()
     {
         $this->categories = Category::all();
+    }
+
+    public function boot()
+    {
+        $this->withValidator(function ($validator) {
+            if ($validator->fails()) {
+                
+                $this->dispatch("swal", [
+                    "icon" => "error",
+                    "title" => "¡Error!",
+                    "text" => "El formulario contiene errores."
+                ]);
+            }
+        });
+    }
+
+    public function categories()
+    {
+        return Category::where("category_id", $this->category_id)->get();
+    }
+
+    public function store()
+    {
+        $this->validate([
+            "image" => "required|image|max:1024",
+            "product.sku" => "required|unique:products,sku",
+            "product.name" => "required|max:255",
+            "product.description" => "nullable",
+            "product.price" => "required|numeric|min:0",    
+            "product.category_id" => "required|exists:categories,id",
+        ]);
+
+        $this->product["image_path"] = $this->image->store("images/products");
+
+        $product = Product::create($this->product);
+
+        session()->flash("swal", [
+            "icon" => "success",
+            "title" => "¡Bien hecho!",
+            "text" => "Producto creado correctamente."
+        ]);
+
+        return redirect()->route("admin.products.edit", $product);
     }
 
     public function render()
--- resources/views/livewire/admin/products/product-create.blade.php
+++ resources/views/livewire/admin/products/product-create.blade.php
@@ -1,44 +1,66 @@
-<div class="card">
+<div>
+    <form wire:submit="store">
+        <figure class="mb-4 relative w-full h-[900px]"> <!-- Establece una altura fija -->
+            <div class="absolute top-8 right-8">
+                <label class="flex items-center px-4 py-2 rounded-lg bg-white cursor-pointer">
+                    <i class="fa-solid fa-camera-retro mr-2"></i>
+                    Actualizar imagen
+                    <input type="file" class="hidden" accept="image/*" wire:model="image">
+                </label>
+            </div>
 
-    <div class="mb-4">
-        <x-label class="mb-1">
-            Código
-        </x-label>
+            <img class="w-full h-full object-cover object-center"
+                src="{{ $image ? $image->temporaryUrl() : asset('images/notavailable.jpeg') }}" alt="">
+        </figure>
 
-        <x-input wire:model="product.sku" class="w-full" placeholder="Por favor ingrese el código del producto" /> 
-    </div>
+        <x-validation-errors class="mb-4" />
 
-    <div class="mb-4">
-        <x-label class="mb-1">
-            Nombre
-        </x-label>
+        <div class="card">
+            <div class="mb-4">
+                <x-label class="mb-1">Código</x-label>
+                <x-input wire:model="product.sku" class="w-full" placeholder="Por favor ingrese el código del producto" />
+            </div>
 
-        <x-input wire:model="product.name" class="w-full" placeholder="Por favor ingrese el nombre del producto" />
-    </div>
+            <div class="mb-4">
+                <x-label class="mb-1">Nombre</x-label>
+                <x-input wire:model="product.name" class="w-full" placeholder="Por favor ingrese el nombre del producto" />
+            </div>
 
-    <div class="mb-4">
-        <x-label class="mb-1">
-            Descripción
-        </x-label>
-        
-        <x-textarea wire:model="product.description" class="w-full" placeholder="Por favor ingrese la descripción del producto" />
-    </div>
+            <div class="mb-4">
+                <x-label class="mb-1">Descripción</x-label>
+                <x-textarea wire:model="product.description" class="w-full"
+                    placeholder="Por favor ingrese la descripción del producto" />
+            </div>
 
-    <div class="mb-4">
-        <x-label class="mb-1">
-            Categorias
-        </x-label>
+            <div class="mb-4">
+                <x-label class="mb-1">Categorias</x-label>
+                <x-select class="w-full" wire:model.live="product.category_id">
+                    <option value="" disabled>Seleccione una categoría</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </x-select>
+            </div>
 
-        <x-select class="w-full">
-            
-            @foreach ($categories as $category)
-                <option value="{{ $category->id }}">
-                    {{ $category->name }} 
-                </option>
-            @endforeach
+            <div class="mb-4">
+                <x-label class="mb-1">
+                    Precio
+                </x-label>
 
-        </x-select>
-    </div>
+                <x-input
+                    type="number"
+                    step="0.01" 
+                    wire:model="product.price"
+                    class="w-full"
+                    placeholder="Por favor ingrese el precio del producto"/>
+            </div>
 
+            <div class="flex justify-end">
+                <x-button>
+                    Crear producto
+                </x-button>
+            </div>
+
+        </div>
+    </form>
 </div>
-
